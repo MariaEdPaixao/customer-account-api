0c81db4f2db35286d8483c0d7ef039e8cb9c6a18
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2026-06-04 21:12:53 -0300

    chore: initialize customer account API project

---
+++ src/test/java/com/customeraccountapi/CustomerAccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customeraccountapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAccountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
